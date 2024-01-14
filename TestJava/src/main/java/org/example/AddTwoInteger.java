b04b2f79911bdc8095e7bdd14719c6ff9ad379a2
Rakesh Kumar <107539160+rakum23@users.noreply.github.com>
2024-01-14 13:29:14 +0100

    Added sum method

--- TestJava/src/main/java/org/example/AddTwoInteger.java
+++ TestJava/src/main/java/org/example/AddTwoInteger.java
@@ -5,7 +5,7 @@ import static java.lang.Long.sum;
 public class AddTwoInteger {
 
   public long addAndReturn(int a, int b) {
-    System.out.println("Hello and welcome Anju git action");
+    System.out.println("Add and Return method!!!!");
     return sum(a ,b);
   }
 
